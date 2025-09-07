b3ad32b56b087050150f727bcadfb0bf6ac3cfab
Harshitaaery <aeryharshita61@gmail.com>
2025-09-07 11:29:09 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',   // ✅ important for Vercel
+  base: '/EventCraft_main',   // ✅ important for Vercel
 })
